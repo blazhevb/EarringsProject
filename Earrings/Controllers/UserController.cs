7c41622279e8ad36f0eae153a4ad279a261463f1
Borislav Blazhev <b.d.blazhev@gmail.com>
2018-10-26 02:13:20 +0300

    Initial commit

---
+++ Earrings/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Earrings.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Registration form.";
+
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Login form.";
+
+            return View();
+        }
+    }
+}
