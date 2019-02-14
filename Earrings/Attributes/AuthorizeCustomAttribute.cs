71d4d8493883e50c9086fd7b2db9e4e2170f5358
Borislav Blazhev <Borislav.Blazhev@dswe.local>
2019-02-14 18:19:36 +0200

    changes

--- Earrings/Attributes/AuthorizeCustomAttribute.cs
+++ Earrings/Attributes/AuthorizeCustomAttribute.cs
@@ -14,9 +14,7 @@ namespace Earrings.Attributes
 {
     public class AuthorizeCustomAttribute : AuthorizeAttribute
     {
-        private string username;
-        private string token;
-        private string requestToken;
+
 
         public override void OnAuthorization(AuthorizationContext filterContext)
         {
@@ -24,39 +22,9 @@ namespace Earrings.Attributes
             {
                 return;
             }
-            GetUsername();
-            AuthenticationManager mgr = new AuthenticationManager(HttpContext.Current);
-            if(this.token == null)
-            {
-                this.token = mgr.GetTokenFromDb(this.username);
-                if(this.token.Equals(this.requestToken));
-                IIdentity identity = new GenericIdentity(this.username);
-                IPrincipal principal = new GenericPrincipal(identity, new string[] { "user" });
-                HttpContext.Current.User = principal;
-            }
-        }
-
-        protected override bool AuthorizeCore(HttpContextBase httpContext)
-        {
-
-            if(this.token != null && this.token.Equals(this.requestToken))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private void GetUsername()
-        {
-            string[] cookieValues = HttpContext.Current.Request.Cookies["authtcn"].Value.Split(':');
-            if(cookieValues.Length == 2)
-            {
-                this.username = cookieValues[0];
-                this.requestToken = cookieValues[1];
-            }           
+            AuthenticationModule module = new AuthenticationModule();
+            module.Authenticate();
+            
         }
 
         private static bool SkipAuthorization(ActionDescriptor actions)
---
+++ EarringsBusinessLogic/Authentication/AuthenticationModule.cs
@@ -0,0 +1,52 @@
+﻿using EarringsBusinessLogic.Authentication.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EarringsBusinessLogic.Authentication
+{
+    public class AuthenticationModule : IAuthenticator
+    {
+        private string username;
+        private string token;
+        private string requestToken;
+
+        public bool Authenticate()
+        {
+            GetUsername();
+            AuthenticationManager mgr = new AuthenticationManager(HttpContext.Current);
+            if(this.token == null)
+            {
+                this.token = mgr.GetTokenFromDb(this.username);
+                if(this.token.Equals(this.requestToken))
+                {
+                    UpdatePrincipal(this.username, new string[] { "user" });
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        private void GetUsername()
+        {
+            string[] cookieValues = HttpContext.Current.Request.Cookies["authtcn"]?.Value.Split(':');
+            if(cookieValues != null && cookieValues.Length == 2)
+            {
+                this.username = cookieValues[0];
+                this.requestToken = cookieValues[1];
+            }
+        }
+
+        public void UpdatePrincipal(string username, string[] roles)
+        {
+            IIdentity identity = new GenericIdentity(username);
+            IPrincipal principal = new GenericPrincipal(identity, roles);
+            HttpContext.Current.User = principal;
+        }
+    }
+}
